9880699b21d60b3224dc65b7026b8f55e62f7629
sastriodjamier <sastriodjamier@gmail.com>
2023-07-06 20:55:38 +0700

    add function to show button date range

--- dags/automate_sales_bar_chart.py
+++ dags/automate_sales_bar_chart.py
@@ -33,6 +33,18 @@ def read_query_file():
         query = file.read()
     return query
 
+def get_date_range():
+    default_end_date = pendulum.today().date()
+    default_start_date = default_end_date.start_of('month')
+        
+    dates = st.date_input(label='Dates', value=(default_start_date, default_end_date), key='date_input')
+
+    if len(dates) != 2:
+        st.stop()
+
+    start_date, end_date = dates
+    return start_date, end_date
+
 def execute_query(query):
     conn = get_snowflake_connection()
     result = pd.read_sql(query, conn)
