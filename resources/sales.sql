d203b01567e914f833eddb0457c1038be2745f3b
sastriodjamier <sastriodjamier@gmail.com>
2023-09-30 23:14:27 +0700

    delete dag

--- resources/sales.sql
+++
@@ -1,8 +0,0 @@
-select
-    order_source
-    , sum(price) as total_sales
-from fact_orders
-where
-    order_date between {start_date_str}::timestamp and {end_date_str}::timestamp
-    and order_status = 'Completed'
-group by 1
